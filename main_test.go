b65abf17b2552b31c68158ee060397ad2cc6666b
Gerald <me@jacobford.co.uk>
2021-01-29 12:46:44 +0000

    added benchmark test

---
+++ main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"testing"
+	"io/ioutil"
+	"strings"
+	"sync"
+	gg "gogasm/src"
+)
+
+func BenchmarkRunner(b *testing.B) {
+	r := gg.NewRunner("tcp", "127.0.0.1:3000", "server/big.txt")
+
+	data, _ := ioutil.ReadFile("server/big.txt")
+	lines := strings.Split(string(data), "\n")
+
+	var wg sync.WaitGroup
+	for n := 0; n <= b.N; n++ {
+		go r.DoRequest(lines[n], &wg)
+	}
+	wg.Wait()
+}
